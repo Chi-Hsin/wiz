8b9b66a30b6d61637a85cbf3be3be382d39c63a6
Chi-Hsin <jack1234552000@yahoo.com.tw>
2020-03-23 20:51:42 +0800

    202003232050
    初步架構

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var indexData = new Vue({
+            el: '#outerDiv',
+            data: {
+                "eventDiv":{
+                    "eventTitle":{
+                        "width":"300px"
+                    },
+                    "eventArrow":{
+                        "margin":"auto"
+                    },
+                    "eventNumber":{
+                        "margin":"auto",
+                        "fontWeight":"bold"
+                    }
+                }
+                 
+               }
+           })

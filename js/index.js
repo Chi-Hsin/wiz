0ca82df9a558ccb3c7cf89926beab864f1f033f2
HSIN <jack1234552000@yahoo.com.tw>
2022-07-07 12:06:51 +0800

    20220707
    新活動更新

--- js/index.js
+++ js/index.js
@@ -607,24 +607,15 @@ var indexData = new Vue({
 					}
 					
 				});
-				ClassicEditor
-				.create( document.querySelector( '#editor' ), {
-					toolbar: [ 'heading', '|', 'bold', 'link', 'bulletedList', 'numberedList', 'blockQuote' ,'fontColor'],
-					heading: {
-						options: [
-							{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-							{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-							{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-						]
-					},
-					
-				} )
-				.then(function(editor){
-					indexData.editor = editor;
-				})
-				.catch( error => {
-					console.log( error );
-				} );
+				  window.onbeforeunload = function (e) {
+				  e = e || window.event;
+				  // 兼容IE8和Firefox 4之前的版本
+				  if (e) {
+					  e.returnValue = '关闭提示';
+				  }
+				  // Chrome, Safari, Firefox 4+, Opera 12+ , IE 9+
+				  return '关闭提示';
+			  };
 				
 				
             },
